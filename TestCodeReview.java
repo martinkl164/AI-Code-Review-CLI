f26492bf7f1aec67d22b4be4a8ac2eb41aaf7164
Martin Klapec <martin_klapec@epam.com>
2026-01-28 14:16:38 +0100

    Test improved AI review with JSON output

--- TestCodeReview.java
+++
@@ -1,9 +0,0 @@
-public class TestCodeReview {
-    // ISSUE: Hardcoded secret
-    private static final String API_KEY = "sk-test-12345";
-    
-    // ISSUE: SQL Injection
-    public void getUser(String username) {
-        String query = "SELECT * FROM users WHERE name = '" + username + "'";
-    }
-}
